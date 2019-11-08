32f7cb01cb148eb9948892e38180d461e4b9921b
Razi <razikallayi@gmail.com>
2019-11-08 20:38:01 +0300

    add cloudant

--- src/main.js
+++ src/main.js
@@ -14,7 +14,9 @@ PouchDB.plugin(PouchDBAuth);
 // const baseUrl = 'http://127.0.0.1:5984'
 // const baseUrl = 'https://library.smileupps.com'
 // const baseUrl = 'https://6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix.cloudant.com'
-const baseUrl = 'https://6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix.cloudantnosqldb.appdomain.cloud/'
+// const baseUrl = 'https://6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix.cloudantnosqldb.appdomain.cloud/'
+const baseUrl = 'https://6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix:8f78809b20958e5c888a280f78db04241cad9e1228e6469f4abc09e714d587c0@6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix.cloudantnosqldb.appdomain.cloud'
+// const baseUrl = 'https://0e168c5f-c3ad-4a61-bd36-c91ab4be957d-bluemix:b96f518ba54571c221e803f28f473acd68cb11c15b04d4c1f336bfb07c5289dc@0e168c5f-c3ad-4a61-bd36-c91ab4be957d-bluemix.cloudant.com/'
 export const db = new PouchDB(baseUrl + '/library', { skip_setup: true });
 export const usersDB = new PouchDB(baseUrl + '/_users', { skip_setup: true });
 
@@ -28,3 +30,8 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
+
+// unm:6abf0a2a-f0d4-4418-9401-1a1db0e33f60-bluemix
+// pwd:8f78809b20958e5c888a280f78db04241cad9e1228e6469f4abc09e714d587c0
--- src/router/index.js
+++ src/router/index.js
@@ -15,102 +15,102 @@ Vue.use(Router)
 let router = new Router({
   mode: 'history',
   routes: [
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
-  {
-    path: '/books',
-    name: 'books',
-    component: Books,
-    meta: { requiresAuth: true , roles:['librarian','_admin']}
-  },
-  {
-    path: '/book/:id',
-    name: 'book_details',
-    component: BookDetails,
-    props:true,
-    meta: { requiresAuth: true , roles:['reader','librarian','_admin']}
-  },
-  {
-    path: '/users',
-    name: 'users',
-    component: User,
-    meta: { requiresAuth: true , roles:['_admin']}
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-    meta: { requiresAuth: true , roles:['reader','librarian','_admin']}
-  },
-  {
-    path: '/denied/:message',
-    name: 'error',
-    component: ErrorPage,
-    props: true
-  },
-  {
-    path: '/logout',
-    name: 'logout',
-    component: function () {
-      store.dispatch('userSignOut')
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/books',
+      name: 'books',
+      component: Books,
+      meta: { requiresAuth: true, roles: ['librarian', '_admin'] }
+    },
+    {
+      path: '/book/:id',
+      name: 'book_details',
+      component: BookDetails,
+      props: true,
+      meta: { requiresAuth: true, roles: ['reader', 'librarian', '_admin'] }
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: User,
+      meta: { requiresAuth: true, roles: ['_admin'] }
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      meta: { requiresAuth: true, roles: ['reader', 'librarian', '_admin'] }
+    },
+    {
+      path: '/denied/:message',
+      name: 'error',
+      component: ErrorPage,
+      props: true
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: function () {
+        store.dispatch('userSignOut')
+      }
+    },
+    {
+      path: '*',
+      component: Login
     }
-  },
-  { 
-    path: '*', 
-    component: Login 
-  }
   ]
 })
 
 
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  if(requiresAuth) {
+  if (requiresAuth) {
     db.getSession(function (err, response) {
       if (err) {
-        next({name:'login'})
+        next({ name: 'login' })
       } else if (!response.userCtx.name) {
-        next({name:'login'})
+        next({ name: 'login' })
       } else {
 
-        var user={};
-        user.role = response.userCtx.roles[0]==undefined?'reader':response.userCtx.roles[0];
+        var user = {};
+        user.role = response.userCtx.roles[0] == undefined ? 'reader' : response.userCtx.roles[0];
         user.name = response.userCtx.name;
-        if(user.role!=="_admin"){
+        if (user.role !== "_admin") {
           db.getUser(response.userCtx.name, function (err, response) {
             if (!err) {
-              if(!response.active){
-               store.dispatch('setUser', {})
-               var reason = "Your account is not activated. Please contact admin for suppport."
-               reason= btoa(reason)
-               next({ name: 'error', params: { message: reason }}) 
-               return response
-             }
-           }
-         });
+              if (!response.active) {
+                store.dispatch('setUser', {})
+                var reason = "Your account is not activated. Please contact admin for suppport."
+                reason = btoa(reason)
+                next({ name: 'error', params: { message: reason } })
+                return response
+              }
+            }
+          });
         }
-        
+
         store.dispatch('setUser', user)
 
-        var roleCheck = to.meta.roles.some((auth)=>auth===user.role);
-        if(roleCheck) {
+        var roleCheck = to.meta.roles.some((auth) => auth === user.role);
+        if (roleCheck) {
           next()
-        }else{
+        } else {
           var reason = "You don't have permission. Please contact admin for support."
-          reason= btoa(reason)
-          next({ name: 'error', params: { message: reason }})
+          reason = btoa(reason)
+          next({ name: 'error', params: { message: reason } })
         }
       }
-    }) 
-  }else {
+    })
+  } else {
     next()
   }
 })
